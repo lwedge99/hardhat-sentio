a59cac9e0d21bfd948a33e7b7fc803233ca63e69
longw <lwedge99@gmail.com>
2024-01-12 14:52:34 +0800

    compile before upload

--- src/tasks/upload.ts
+++ src/tasks/upload.ts
@@ -2,6 +2,7 @@ import { task } from 'hardhat/config'
 import { HardhatPluginError } from 'hardhat/plugins'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { PLUGIN_NAME } from '../constants'
+import { TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
 
 task('sentio:upload', 'upload contracts')
   .addPositionalParam(
@@ -17,6 +18,7 @@ async function uploadContracts(
   if (!contracts) {
     throw new HardhatPluginError(PLUGIN_NAME, 'missing contracts')
   }
+  await hre.run(TASK_COMPILE)
   for (const name of (contracts as string).split(',')) {
     await hre.sentio.upload(name)
   }
